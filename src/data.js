7d1dc38fca3f54e96a0803252c3ed1764a65b414
Sumanth <sumanth.koppula05@gmail.com>
2023-06-12 11:49:38 +0530

    chat-app

---
+++ src/data.js
@@ -0,0 +1,207 @@
+export const data = {
+  users: [
+    {
+      id: 1,
+      name: "Rishon Kumar",
+      img_url: "https://cdn-icons-png.flaticon.com/512/265/265674.png",
+      chats: [
+        {
+          my_message: [
+            {
+              file: "",
+              content: "Wassupp",
+              name: "Admin",
+              img_url: "https://cdn-icons-png.flaticon.com/512/265/265674.png",
+            },
+          ],
+          friend_message: [
+            {
+              file: "",
+              content: "Heyyy!!!",
+              name: "Rajav",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/3048/3048122.png",
+            },
+            {
+              file: "",
+              content: "How are you",
+              name: "Rajav",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/3048/3048122.png",
+            },
+            {
+              file: "",
+              content: "xyz",
+              name: "Naveen",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/3048/3048122.png",
+            },
+          ],
+        },
+      ],
+      lastText: "Wassupp",
+    },
+    {
+      id: 2,
+      name: "Virat",
+      img_url: "https://cdn-icons-png.flaticon.com/512/921/921110.png",
+      chats: [
+        {
+          my_message: [
+            {
+              file: "",
+              content: "Hi!Vignesh",
+              name: "Admin",
+              img_url: "https://cdn-icons-png.flaticon.com/512/921/921110.png",
+            },
+            {
+              file: "",
+              content: "How About You?",
+              name: "Admin",
+              img_url: "https://cdn-icons-png.flaticon.com/512/921/921110.png",
+            },
+          ],
+          friend_message: [
+            {
+              file: "",
+              content: "Hey! hi Virat How are?",
+              name: "Vignesh",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/4825/4825096.png",
+            },
+            {
+              file: "",
+              content: "",
+              name: "Vignesh",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/4825/4825096.png",
+            },
+            {
+              file: "",
+              content: "",
+              name: "Vignesh",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/4825/4825096.png",
+            },
+          ],
+        },
+      ],
+      lastText: " How About You?",
+    },
+    {
+      id: 3,
+      name: "Vignesh",
+      img_url: "https://cdn-icons-png.flaticon.com/512/4825/4825096.png",
+      chats: [
+        {
+          my_message: [
+            {
+              file: "",
+              content: "Wait",
+              name: "Admin",
+              img_url: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+            },
+          ],
+          friend_message: [
+            {
+              file: "",
+              content: "please call me",
+              name: "Sakthia",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/4825/4825096.png",
+            },
+          ],
+        },
+      ],
+      lastText: "Wait..",
+    },
+    {
+      id: 4,
+      name: "Preetha",
+      img_url: "https://cdn-icons-png.flaticon.com/512/4825/4825123.png",
+      chats: [
+        {
+          my_message: [
+            {
+              file: "",
+              content: "Hi! Ram",
+              name: "Admin",
+              img_url: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+            },
+          ],
+          friend_message: [
+            {
+              file: "",
+              content: "wassup ",
+              name: "Ran",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/4825/4825123.png",
+            },
+          ],
+        },
+      ],
+      lastText: "Hi! Ram",
+    },
+    {
+      id: 5,
+      name: "Sakthia",
+      img_url: "https://cdn-icons-png.flaticon.com/512/4825/4825112.png",
+      chats: [
+        {
+          my_message: [
+            {
+              file: "",
+              content: "Busy adari niv",
+              name: "Admin",
+              img_url: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+            },
+          ],
+          friend_message: [
+            {
+              file: "",
+              content: "Ella ri",
+              name: "Venkat",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/4825/4825112.png",
+            },
+            {
+              file: "",
+              content: "Mate yenu samachara",
+              name: "Arjun",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/4825/4825112.png",
+            },
+          ],
+        },
+      ],
+      lastText: "Busy adari niv",
+    },
+    {
+      id: 6,
+      name: "Arjun",
+      img_url: "https://cdn-icons-png.flaticon.com/512/4825/4825044.png",
+      chats: [
+        {
+          my_message: [
+            {
+              file: "",
+              content: "gharpe hey re call karta ruk",
+              name: "Admin",
+              img_url: "https://cdn-icons-png.flaticon.com/512/64/64572.png",
+            },
+          ],
+          friend_message: [
+            {
+              file: "",
+              content: "Are bhai kaha hey!",
+              name: "Virat",
+              img_url:
+                "https://cdn-icons-png.flaticon.com/512/4825/4825044.png",
+            },
+          ],
+        },
+      ],
+      lastText: "gharpe hey re call karta ruk",
+    },
+  ],
+};
